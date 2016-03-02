0c88beb3f4caa492ed1b22818b916033686d03d6
Jamshid <qq@qq.com>
2016-03-02 12:08:50 -0800

    Added storage mem cache blank stub functions.

--- src/Blockchain/DB/StorageDB.hs
+++ src/Blockchain/DB/StorageDB.hs
@@ -32,7 +32,59 @@ class MonadResource m=>
 
 putStorageKeyVal'::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
                   Address->Word256->Word256->m ()
-putStorageKeyVal' owner key val = do
+putStorageKeyVal' owner key val = putStorageKeyValMC owner key val
+
+deleteStorageKey'::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+                   Address->Word256->m ()
+deleteStorageKey' owner key = deleteStorageKeyMC owner key
+
+getStorageKeyVal'::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+                   Address->Word256->m Word256
+getStorageKeyVal' owner key = getStorageKeyValMC owner key
+
+getAllStorageKeyVals'::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+                       Address->m [(MP.Key, Word256)]
+getAllStorageKeyVals' owner = getAllStorageKeyValsMC owner
+
+
+
+
+
+
+--The following are the memory cache versions of the functions
+
+putStorageKeyValMC::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+                  Address->Word256->Word256->m ()
+putStorageKeyValMC owner key val = putStorageKeyValDB owner key val
+
+deleteStorageKeyMC::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+                   Address->Word256->m ()
+deleteStorageKeyMC owner key = deleteStorageKeyDB owner key
+
+getStorageKeyValMC::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+                   Address->Word256->m Word256
+getStorageKeyValMC owner key = getStorageKeyValDB owner key
+
+getAllStorageKeyValsMC::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+                       Address->m [(MP.Key, Word256)]
+getAllStorageKeyValsMC owner = getAllStorageKeyValsDB owner
+
+
+
+
+
+
+
+
+
+
+
+--The following are the DB versions of the functions
+
+
+putStorageKeyValDB::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+                  Address->Word256->Word256->m ()
+putStorageKeyValDB owner key val = do
   hashDBPut storageKeyNibbles
   addressState <- getAddressState owner
   db <- fmap fst getStorageDB
@@ -41,18 +93,18 @@ putStorageKeyVal' owner key val = do
   putAddressState owner addressState{addressStateContractRoot=newContractRoot}
   where storageKeyNibbles = N.pack $ (N.byte2Nibbles =<<) $ word256ToBytes key
 
-deleteStorageKey'::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+deleteStorageKeyDB::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
                    Address->Word256->m ()
-deleteStorageKey' owner key = do
+deleteStorageKeyDB owner key = do
   addressState <- getAddressState owner
   db <- fmap fst getStorageDB
   let mpdb = MP.MPDB{MP.ldb=db, MP.stateRoot=addressStateContractRoot addressState}
   newContractRoot <- fmap MP.stateRoot $ MP.deleteKey mpdb (N.pack $ (N.byte2Nibbles =<<) $ word256ToBytes key)
   putAddressState owner addressState{addressStateContractRoot=newContractRoot}
 
-getStorageKeyVal'::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+getStorageKeyValDB::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
                    Address->Word256->m Word256
-getStorageKeyVal' owner key = do
+getStorageKeyValDB owner key = do
   addressState <- getAddressState owner
   db <- fmap fst getStorageDB
   let mpdb = MP.MPDB{MP.ldb=db, MP.stateRoot=addressStateContractRoot addressState}
@@ -61,9 +113,9 @@ getStorageKeyVal' owner key = do
     Nothing -> return 0
     Just x -> return $ fromInteger $ rlpDecode $ rlpDeserialize $ rlpDecode x
 
-getAllStorageKeyVals'::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
+getAllStorageKeyValsDB::(HasMemAddressStateDB m, HasStorageDB m, HasStateDB m, HasHashDB m)=>
                        Address->m [(MP.Key, Word256)]
-getAllStorageKeyVals' owner = do
+getAllStorageKeyValsDB owner = do
   addressState <- getAddressState owner
   db <- fmap fst getStorageDB
   let mpdb = MP.MPDB{MP.ldb=db, MP.stateRoot=addressStateContractRoot addressState}
