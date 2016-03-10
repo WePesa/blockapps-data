9870f9d47333e335cda8c43e752d8df105873acc
kejace <kejace@gmail.com>
2016-03-10 21:39:01 +0000

    Un-splitted blockapps-data-db

---
+++ exec_src/Main.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+--import HFlags
+
+import Blockchain.Setup
+
+--defineFlag "genesisBlockName" "canonical" "use the alternate stablenet genesis block"
+-- $(return []) --see https://github.com/nilcons/hflags/issues/8
+
+main::IO ()
+main = do
+ -- _ <- $initHFlags "Setup EthereumH DBs"
+  putStrLn "hello strato-setup"
+  oneTimeSetup
