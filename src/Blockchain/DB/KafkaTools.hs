8ee3bfa9dd7726c701f2a00c652a211ad2655cd2
Jamshid <qq@qq.com>
2016-03-10 20:17:46 -0800

    Adding function to do a Kafka fetch.

---
+++ src/Blockchain/DB/KafkaTools.hs
@@ -0,0 +1,26 @@
+
+module Blockchain.DB.KafkaTools (
+  fetchBytes
+  ) where
+
+import qualified Data.ByteString as B
+import Data.Maybe
+
+import Network.Kafka
+import Network.Kafka.Consumer
+import Network.Kafka.Protocol hiding (Message)
+
+
+fourth4::(a,b,c,d)->d
+fourth4 (_, _, _, x) = x
+
+fifth5::(a,b,c,d,e)->e
+fifth5 (_, _, _, _, x) = x
+
+fetchBytes::Offset->TopicName->Kafka [B.ByteString]
+fetchBytes offset topic = do
+  result <- fetch (Offset $ fromIntegral offset) 0 topic
+  return $ concat $ map (map (_kafkaByteString . fromJust . _valueBytes . fifth5 . _messageFields .  _setMessage)) $ map _messageSetMembers $ map fourth4 $ head $ map snd $ _fetchResponseFields result
+
+
+
