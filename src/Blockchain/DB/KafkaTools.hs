7da6a369581bd52eb5782dae953746b37415f329
Jamshid <qq@qq.com>
2016-03-13 14:58:48 -0700

    Cleanup.

--- src/Blockchain/DB/KafkaTools.hs
+++ src/Blockchain/DB/KafkaTools.hs
@@ -10,17 +10,10 @@ import Network.Kafka
 import Network.Kafka.Consumer
 import Network.Kafka.Protocol hiding (Message)
 
-
-fourth4::(a,b,c,d)->d
-fourth4 (_, _, _, x) = x
-
-fifth5::(a,b,c,d,e)->e
-fifth5 (_, _, _, _, x) = x
-
 fetchBytes::TopicName->Offset->Kafka [B.ByteString]
 fetchBytes topic offset = do
   result <- fetch (Offset $ fromIntegral offset) 0 topic
-  return $ concat $ map (map (_kafkaByteString . fromJust . _valueBytes . fifth5 . _messageFields .  _setMessage)) $ map _messageSetMembers $ map fourth4 $ head $ map snd $ _fetchResponseFields result
+  return $ map tamPayload $ fetchMessages result
 
 
 
