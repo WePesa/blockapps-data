0997d4e5dfecb5b9b948368d06ced6301b0b7157
kjameslubin <kieren1@gmail.com>
2016-05-14 01:08:28 -0400

    transactionRoot really working

--- src/Blockchain/Verification.hs
+++ src/Blockchain/Verification.hs
@@ -8,16 +8,40 @@ module Blockchain.Verification (
 
 import Prelude.Unicode
 
+import Control.Monad.Trans.State
+
 import Blockchain.Data.BlockDB
 import Blockchain.Data.RLP
 import Blockchain.Data.Transaction
 import qualified Blockchain.Database.MerklePatricia as MP
 import Blockchain.SHA
+import Blockchain.Database.MerklePatricia.InternalMem
+import Blockchain.Database.MerklePatriciaMem
+import Blockchain.Database.KeyVal
+import Blockchain.Util
 
+import Data.NibbleString
 
-
+import Data.Functor.Identity
+{-
 transactionsVerificationValue::[Transaction]->MP.StateRoot
 transactionsVerificationValue = MP.sha2StateRoot . listToRLPVerificationValue
+-}
+
+
+addAllKVsMem::RLPSerializable obj=>Monad m=>MPMem->[(Integer, obj)]->m MPMem
+addAllKVsMem x [] = return x
+addAllKVsMem mpdb (x:rest) = do
+  mpdb' <- unsafePutKeyValMem mpdb (byteString2NibbleString $ rlpSerialize $ rlpEncode $ fst x) (rlpEncode $ rlpSerialize $ rlpEncode $ snd x)
+  addAllKVsMem mpdb' rest
+
+blank :: MPMem
+blank = initializeBlankMem { mpStateRoot = MP.emptyTriePtr }
+
+transactionsVerificationValue::[Transaction]->MP.StateRoot
+transactionsVerificationValue theList = runIdentity $ do
+    mp <- addAllKVsMem blank $ zip [0..] $ theList
+    return (mpStateRoot mp)
 
 ommersVerificationValue::[BlockData]->SHA
 ommersVerificationValue = listToRLPVerificationValue 
