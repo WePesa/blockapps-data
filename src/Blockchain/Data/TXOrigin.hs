cd460c58abad70cc1c2a0b113ff8748ca3396fdd
Ilya Ostrovskiy <ilya@blockapps.net>
2016-11-29 17:25:50 -0500

    stashing changes

--- src/Blockchain/Data/TXOrigin.hs
+++ src/Blockchain/Data/TXOrigin.hs
@@ -2,6 +2,9 @@
     
 module Blockchain.Data.TXOrigin where
 
+import Data.Binary
+import Data.Binary.Get
+import Data.Binary.Put
 
 import Database.Persist.TH
 
@@ -12,6 +15,7 @@ import Blockchain.MiscJSON ()
 
 import Blockchain.SHA
 
+import qualified GHC.Word as GW
 import GHC.Generics
 
 data TXOrigin = Direct | API | BlockHash SHA | PeerString String deriving (Show, Read, Eq, Generic)
@@ -21,5 +25,15 @@ derivePersistField "TXOrigin"
 instance ToJSON TXOrigin where
 instance FromJSON TXOrigin where
 
-
-  
+instance Binary TXOrigin where
+    put Direct          = putWord8 0
+    put API             = putWord8 1
+    put (BlockHash sha) = putWord8 2 >> put sha
+    put (PeerString p)  = putWord8 3 >> put p
+    get = do
+        tag <- getWord8
+        case tag of
+            0 -> return Direct
+            1 -> return API
+            2 -> BlockHash  <$> get
+            3 -> PeerString <$> get
